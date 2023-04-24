4ad3123540850e3fdee8f18443cba756af4a42ff
Luis Guilherme <luislebert01@outlook.com>
2023-04-24 13:57:23 -0300

    Classe de configuração criada

---
+++ TurismoMongoDB/Config/MongoDBSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TurismoMongoDB.Config
+{
+    public class MongoDBSettings : IMongoDBSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
