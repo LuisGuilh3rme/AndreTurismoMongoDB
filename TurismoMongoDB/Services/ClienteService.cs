209bbbe78f158b700de0b02736f7bad1b025abc8
Luis Guilherme <luislebert01@outlook.com>
2023-04-24 14:55:39 -0300

    ClienteService criado

---
+++ TurismoMongoDB/Services/ClienteService.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using TurismoMongoDB.Config;
+using TurismoMongoDB.Models;
+
+namespace TurismoMongoDB.Services
+{
+    public class ClienteService
+    {
+        private readonly IMongoCollection<Cliente>? _cliente;
+
+        public ClienteService(IMongoDBSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _cliente = database.GetCollection<Cliente>("cliente");
+        }
+
+        public List<Cliente> Get() => _cliente.Find(c => true).ToList();
+        public Cliente Get(string id) => _cliente.Find(c => c.Id == id).FirstOrDefault();
+
+        public void Post(Cliente cliente)
+        {
+            _cliente.InsertOne(cliente);
+        }
+
+        public Cliente Update(Cliente cliente)
+        {
+            _cliente.ReplaceOne(c => c.Id == cliente.Id, cliente);
+            return cliente;
+        }
+
+        public void Delete(string id)
+        {
+            _cliente.DeleteOne(c => c.Id == id);
+        }
+    }
+}
